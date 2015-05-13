5c0fc21cc0715ce67ce44756db5b49ac874be894
zubiri <zubiri@zubiri-VirtualBox.(none)>
2015-05-13 08:39:10 +0200

    mostrar asignaturas bukatuta

--- verAsignaturas.php
+++ verAsignaturas.php
@@ -23,7 +23,7 @@ $asig = mysql_query($sql);
 echo "<table>";
 while ($row = mysql_fetch_assoc($asig)) {
  echo "<tr><td>$row[asig_cod]</td><td>$row[nombre_asig]</td><td>$row[horas]</td><td>";
- echo "<a href='eliminarAlumnos.php?alu_cod=$row[alu_cod]'>Eliminar</a> <a href='modificarAlumnos.php?alu_cod=$row[alu_cod]'>Editar</a></td></tr>";
+ echo "<a href='eliminarAsignaturas.php?asig_cod=$row[asig_cod]'>Eliminar</a> <a href='modificarAsignaturas.php?asig_cod=$row[asig_cod]'>Editar</a></td></tr>";
 };
 echo "</table>";
 
@@ -37,7 +37,7 @@ echo "FIN DE LAS ASIGNATURAS<hr/>";
 
 mysql_close($dp);
 ?>
-<a href="nuevoAlumno.php" target="">Nuevo Alumno</a><br/>
+<a href="nuevaAsignatura.php" target="">Nueva Asignatura</a><br/>
 <a href="nagusia.html">Inicio</a>
 </body>
 </html>
