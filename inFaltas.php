ac47d2f269553237e2824fe2c624710b631d21a2
oalba <zubiri@zubiri-VirtualBox.(none)>
2015-05-20 12:00:46 +0200

    faltak falta

--- inFaltas.php
+++ inFaltas.php
@@ -2,19 +2,19 @@
 
 $asig = $_GET['asig_cod'];
 $faltas = $_GET['faltas'];
+$fecha = date("Y-m-d");
 
 $dp = mysql_connect("localhost", "root", "zubiri" );
 mysql_select_db("ikastola", $dp);
 
-$calidad=$_POST['calidad'];
-for($i=0;$i<count($calidad);$i++){
-$sql_query = "insert into datos (calidad) values ('$calidad[$i]')" ;
+//$calidad=$_POST['calidad'];
+for($i=0;$i<count($faltas);$i++){
+//$sql_query = "insert into datos (calidad) values ('$calidad[$i]')" ;
+$sql = "INSERT INTO faltar_a_a (asig_cod,alu_cod,fecha) VALUES ($asig,'$faltas[$i]','$fecha')";
+mysql_query($sql);
 }
 
-
-$sql = "INSERT INTO faltar_a_a (asig_cod, alu_cod) VALUES ($asig,$alu)";
-mysql_query($sql);
 mysql_close($dp);
 
-header("location:hasiera.html");
+header("location:clases.php");
 ?>
